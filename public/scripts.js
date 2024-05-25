2134e51845d7237d48dc93eed246e4b117de1553
sergiokingone <sergiorobayorr@gmail.com>
2024-05-25 17:38:24 -0500

    enchanced animations, hero section, etc

---
+++ public/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const themeToggle = document.getElementById('theme-toggle');
+    themeToggle.addEventListener('change', () => {
+      if (themeToggle.checked) {
+        document.body.classList.add('light-theme');
+      } else {
+        document.body.classList.remove('light-theme');
+      }
+    });
+  });
+  
