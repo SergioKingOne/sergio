48f3bd08460491887603fdcd35131ce866968b67
sergiokingone <sergiorobayorr@gmail.com>
2024-05-27 22:42:51 -0500

    contact section improvements

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,28 +7,41 @@ const Contact = () => {
       <div class="contact-card">
         <h2>Contact Me</h2>
         <form id="contact-form">
-          <input type="text" id="name" name="name" placeholder="Your Name" />
+          <label for="name">Your Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your Name"
+            required
+          />
+          <label for="email">Your Email</label>
           <input
             type="email"
             id="email"
             name="email"
             placeholder="Your Email"
+            required
           />
+          <label for="message">Your Message</label>
           <textarea
             id="message"
             name="message"
             placeholder="Your Message"
             rows="5"
+            required
           ></textarea>
           <button type="submit">Send</button>
         </form>
         <div class="contact-info">
           <p>
             Email:{" "}
-            <a href="mailto:youremail@example.com">sergiorobayorr@gmail.com</a>
+            <a href="mailto:sergiorobayorr@gmail.com">
+              sergiorobayorr@gmail.com
+            </a>
           </p>
           <p>
-            Phone: <a href="tel:+57 3114919504">+57 3114919504</a>
+            Phone: <a href="tel:+573114919504">+57 3114919504</a>
           </p>
         </div>
       </div>
