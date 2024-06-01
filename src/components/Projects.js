8adeb457551e5d78859ce64fa4121df67d28ebe6
sergiokingone <sergiorobayorr@gmail.com>
2024-05-31 22:50:34 -0500

    enhanced project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,42 +7,52 @@ const Projects = () => {
       <h2 className="section-title">Projects</h2>
       <div className="projects-list">
         <div className="project-card">
-          <img
-            src="./assets/wild_oasis.jpeg"
-            alt="Project 1 Image"
-            className="project-image"
-          />
+          <div className="project-image-container">
+            <img
+              src="./assets/wild_oasis.jpeg"
+              alt="Wild Oasis"
+              className="project-image"
+            />
+            <div className="overlay">
+              <a
+                href="https://github.com/yourusername/portfolio-website"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="view-code"
+              >
+                View Code
+              </a>
+            </div>
+          </div>
           <div className="project-content">
             <h3>Wild Oasis</h3>
             <p>A personal portfolio website built with HTML, CSS, and React.</p>
-            <a
-              href="https://github.com/yourusername/portfolio-website"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View Code
-            </a>
           </div>
         </div>
         <div className="project-card">
-          <img
-            src="assets/fast-pizza.jpeg"
-            alt="Project 2 Image"
-            className="project-image"
-          />
+          <div className="project-image-container">
+            <img
+              src="./assets/fast-pizza.jpeg"
+              alt="Fast Pizza"
+              className="project-image"
+            />
+            <div className="overlay">
+              <a
+                href="https://github.com/yourusername/forex-trading-bot"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="view-code"
+              >
+                View Code
+              </a>
+            </div>
+          </div>
           <div className="project-content">
             <h3>Fast Pizza</h3>
             <p>
               An automated trading bot developed using Python and various
               trading algorithms.
             </p>
-            <a
-              href="https://github.com/yourusername/forex-trading-bot"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View Code
-            </a>
           </div>
         </div>
       </div>
