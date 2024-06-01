24f16f64409f434d4204c5354e4669e71a186cc1
sergiokingone <sergiorobayorr@gmail.com>
2024-05-31 21:39:05 -0500

    highlight key words

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,41 +8,46 @@ const Experience = () => {
       <div className="timeline">
         <div className="timeline-item">
           <div className="timeline-content">
-            <h3>MLOps Leader at Vertex Studio</h3>
+            <h3>
+              <strong>MLOps Leader</strong> at Vertex Studio
+            </h3>
             <p>
               <strong>Duration:</strong> Jan 2024 - Present
             </p>
             <ul>
               <li>
-                Achieved MLOps leadership within my 3rd month of joining the
-                company.
+                Achieved <strong>MLOps leadership</strong> within my 3rd month
+                of joining the company.
               </li>
               <li>
-                Partly responsible for the development and maintenance of the
-                MLOps pipeline.
+                Partly responsible for the development and maintenance of the{" "}
+                <strong>MLOps pipeline</strong>.
               </li>
             </ul>
           </div>
         </div>
         <div className="timeline-item">
           <div className="timeline-content">
-            <h3>Software Engineer at Vertex Studio</h3>
+            <h3>
+              <strong>Software Engineer</strong> at Vertex Studio
+            </h3>
             <p>
               <strong>Duration:</strong> Nov 2023 - Jan 2024
             </p>
             <ul>
               <li>
-                Developed and maintained endpoints and scripts for AI texture
-                generation and 3D asset generation pipelines using Python and
+                Developed and maintained endpoints and scripts for{" "}
+                <strong>AI texture generation</strong> and{" "}
+                <strong>3D asset generation pipelines</strong> using Python and
                 Blender.
               </li>
               <li>
-                Contributed to the development of APIs using both Python and
-                Rust.
+                Contributed to the development of <strong>APIs</strong> using
+                both Python and Rust.
               </li>
               <li>
-                Utilized Docker and Ansible for containerization and
-                configuration management.
+                Utilized <strong>Docker</strong> and <strong>Ansible</strong>{" "}
+                for containerization and configuration management.
               </li>
             </ul>
           </div>
@@ -55,14 +60,14 @@ const Experience = () => {
             </p>
             <ul>
               <li>
-                Provided timely and effective technical support to customers,
-                assisting in problem-solving issues related to both software and
-                hardware.
+                Provided timely and effective <strong>technical support</strong>{" "}
+                to customers, assisting in problem-solving issues related to
+                both software and hardware.
               </li>
               <li>
                 Collaborated with multifunctional teams to ensure smooth
-                communication between technical support, development, and
-                accounting departments.
+                communication between <strong>technical support</strong>,
+                development, and accounting departments.
               </li>
               <li>
                 Achieved the highest amount of hardest issues solved in a month.
