62275866eec67e2f5a02add29884180501fa8ea2
sergiokingone <sergiorobayorr@gmail.com>
2024-06-05 21:08:59 -0500

    much better about

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,51 @@
 import React from "react";
 import "../styles/about.css";
+import {
+  FaServer,
+  FaCheckDouble,
+  FaTachometerAlt,
+  FaUsers,
+} from "react-icons/fa";
 
 const About = () => {
   return (
     <section id="about" className="about-section section">
       <div className="container">
         <h2 className="section-title">About Me</h2>
+        <div className="features">
+          <div className="feature">
+            <FaServer className="feature-icon" />
+            <h3>Scalable</h3>
+            <p>
+              My highest priority is ensuring our systems can handle increasing
+              loads and scale effortlessly as our product grows.
+            </p>
+          </div>
+          <div className="feature">
+            <FaCheckDouble className="feature-icon" />
+            <h3>Reliable</h3>
+            <p>
+              I focus on building robust and fault-tolerant systems, ensuring
+              high availability and consistent performance.
+            </p>
+          </div>
+          <div className="feature">
+            <FaTachometerAlt className="feature-icon" />
+            <h3>Efficient</h3>
+            <p>
+              I prioritize optimizing algorithms and processes to enhance system
+              performance and reduce resource consumption.
+            </p>
+          </div>
+          <div className="feature">
+            <FaUsers className="feature-icon" />
+            <h3>Collaborative</h3>
+            <p>
+              I foster teamwork and clear communication across departments,
+              ensuring seamless integration and efficient operations.
+            </p>
+          </div>
+        </div>
         <div className="about-content">
           <figure className="about-photo">
             <img src="./assets/images/sergiov2.jpeg" alt="Portrait of Sergio" />
@@ -16,23 +56,39 @@ const About = () => {
               strong background in technical support and a growing enthusiasm
               for software development.
             </p>
-            <p>
-              I currently work as a junior backend developer with Rust and
-              Python. I also have a keen interest in forex day trading, playing
-              video games, and coding.
-            </p>
-            <p>
-              My main goals include becoming a senior software engineer,
-              achieving economic freedom, and maintaining good health.
-            </p>
             <div className="skills">
               <h3>Skills</h3>
               <ul>
-                <li>Python</li>
-                <li>Rust</li>
-                <li>JavaScript</li>
-                <li>Forex Trading</li>
-                <li>Game Development</li>
+                <li>
+                  <span>Python</span>
+                  <div className="bar">
+                    <div className="level" style={{ width: "90%" }}></div>
+                  </div>
+                </li>
+                <li>
+                  <span>Rust</span>
+                  <div className="bar">
+                    <div className="level" style={{ width: "85%" }}></div>
+                  </div>
+                </li>
+                <li>
+                  <span>JavaScript</span>
+                  <div className="bar">
+                    <div className="level" style={{ width: "80%" }}></div>
+                  </div>
+                </li>
+                <li>
+                  <span>Forex Trading</span>
+                  <div className="bar">
+                    <div className="level" style={{ width: "75%" }}></div>
+                  </div>
+                </li>
+                <li>
+                  <span>Game Development</span>
+                  <div className="bar">
+                    <div className="level" style={{ width: "70%" }}></div>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
